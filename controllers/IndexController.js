4c47caf62045fa2d0cf077744c3f0cd7c4ef1af7
Yasha32 <yakov.zverev32@gmail.com>
2023-03-06 13:22:22 +0200

    Created a search method without a validation

--- controllers/IndexController.js
+++ controllers/IndexController.js
@@ -2,23 +2,85 @@
 
 const Item = require('./../models/').Item;
 const Category = require('./../models/').Category;
-const sequelize = require('sequelize');
+const Company = require('./../models/').Company;
+const { Op } = require('sequelize');
 
 exports.index = async function(req, res) {
 
     try {
         
-        let items = await Item.findAll({
-            include: [{ model: Category, as: 'category' }],
-            attributes: {
-                exclude: ['category_id']
-            }
-        });
-        let categories = await Category.findAll();
+        console.log('REQ query ----------  ', req.query);
+        console.log('tooo number -------', typeof +req.query.priceFrom);
 
-        res.render('index', {items, categories});
+        let sortField = 'price';
+        let sortType = 'ASC';
+
+        if(req.query.sort) {
+            let sort = req.query.sort.split('-');
+            sortField = sort[0];
+            sortType  = sort[1];
+            console.log('SORT FIELD : ', sortField, 'SORT TYPE : ', sortType);
+        }
+        
+        // console.log('sort ----- ', sort);
+
+        const categories = await Category.findAll();
+        const companies  = await Company.findAll({
+            raw: true
+        });
+
+        const companiesTitles = companies.map(item => item.title);
+        
+    
+        let items = await Item.findAll({
+            //attributes: ['title', 'price'],
+            include: [ {model: Category, as: 'category'}, {model: Company, as: 'company'}],
+            where: {
+                [Op.and] : [
+                    {
+                        [Op.or] : [
+                            {
+                                title: {
+                                    [Op.iLike] : req.query.search ? '%'+req.query.search+'%' : '%'+ '' +'%',
+                            }},
+                            {
+                                description: {
+                                    [Op.iLike] : req.query.search ? '%'+req.query.search+'%' : '',
+                                }
+                            },
+                        ]
+                    },
+                    {
+                        price: {
+                            [Op.and] : [
+                                {[Op.lte] : +req.query.priceTo ? +req.query.priceTo : 1000000000},
+                                {[Op.gte] : +req.query.priceFrom ? +req.query.priceFrom : 0},
+                            ]
+                    }},
+                    {
+                        '$category.title$' : {
+                            [Op.substring] : req.query.category ? req.query.category : ''
+                        }
+                    },
+                    {
+                        '$company.title$' : {
+                            [Op.in] : req.query.companies ? req.query.companies : companiesTitles
+                        }
+                    },
+
+                ],    
+            },
+            order: [
+                [sortField, sortType]
+            ],
+            //limit: 30 
+        });
+        
+        
+        res.render('index', {items, categories, companies});
         
     } catch (error) {
-        console.log(error);
+        console.log('SEARCH CONTROLLER ERROR ------ ', error);
+        res.send(`SEARCH CONTROLLER ERROR ------ ' ${error}`);
     }
 }
