4c45390850737d70406ce9aadbd1c8ba03fea0da
Yasha32 <yakov.zverev32@gmail.com>
2023-03-06 13:46:07 +0200

    fix: Deleted catalog from header, deleted category controller & router

--- routes/categoryRouter.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-
-const { getItems } = require('./../controllers/CategoryController');
-
-router.get('/:id', getItems);
-
-module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,11 @@
 const router = require('express').Router();
 
-const categoryRouter = require('./categoryRouter');
 const itemRouter = require('./itemRouter');
 
 const {index} = require('./../controllers/IndexController');
 
 router.get('/', index);
 
-router.use('/categories', categoryRouter);
 router.use('/items', itemRouter);
 
 
