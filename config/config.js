aa9224443804404a3fde727db46b742384051450
Yasha32 <yakov.zverev32@gmail.com>
2023-04-01 16:56:43 +0300

    added allowed img formats to config

--- config/config.js
+++ config/config.js
@@ -4,4 +4,10 @@ module.exports = {
     appUrl: process.env.APP_URL,
     port  : process.env.PORT,
     appSecret : process.env.APP_SECRET,
+    imgFormats: {
+        'image/png' : 'png',
+        'image/jpeg' : 'jpeg',
+        'image/jpg' : 'jpg',
+        'image/wepb' : 'wepb'
+    }
 }
