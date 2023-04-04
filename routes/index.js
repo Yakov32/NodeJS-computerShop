50a964a390b29d6daa635b2c8e4876f372165ac1
Yasha32 <yakov.zverev32@gmail.com>
2023-04-04 21:17:32 +0300

    feat: added alreadyAuthorized to auth & regist routers

--- routes/index.js
+++ routes/index.js
@@ -6,15 +6,15 @@ const authRouter = require('./authRouter');
 const profileRouter = require('./profileRouter');
 const commentRouter = require('./commentRoute');
 const likeRouter = require('./likeRouter');
-const {checkUser} = require('./../middleware/authorization');
+const {checkUser, alreadyAuthorized} = require('./../middleware/authorization');
 
 const {index} = require('./../controllers/IndexController');
 
 router.get('/', checkUser, index);
 
 router.use('/items', checkUser, itemRouter);
-router.use('/regist', registRouter);
-router.use('/auth', authRouter);
+router.use('/regist', alreadyAuthorized, registRouter);
+router.use('/auth', alreadyAuthorized, authRouter);
 router.use('/profile', profileRouter);
 router.use('/comments', checkUser, commentRouter);
 router.use('/likes', likeRouter);
