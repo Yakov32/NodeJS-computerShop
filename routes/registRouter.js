7abcb417e676eb029ca2263d5dbde0704d459221
Yasha32 <yakov.zverev32@gmail.com>
2023-05-07 10:09:21 +0300

    feat: refactored validation of registration

--- routes/registRouter.js
+++ routes/registRouter.js
@@ -7,6 +7,6 @@ const errorHandler = require('./../validators/user/errorHandler');
 
 router.get('/', sendForm);
 
-router.post('/', [rules(), validate, errorHandler], registrate);
+router.post('/', [[rules.email, rules.password], validate, errorHandler], registrate);
 
 module.exports = router;
--- validators/user/rules.js
+++ validators/user/rules.js
@@ -2,9 +2,7 @@
 
 const {body} = require('express-validator');
 
-exports.rules = () => {
-    return [
-        body('user_email').isEmail().withMessage('Некорректная почта'),
-        body('user_password').isLength({min: 6}).withMessage('Пароль должен быть >= 6'),
-    ];
+exports.rules =  {
+    email: body('user_email').isEmail().withMessage('Некорректная почта'),
+    password: body('user_password').isLength({min: 6}).withMessage('Пароль должен быть >= 6'),
 }
